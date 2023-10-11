bf6d02e784062d658b8c5c9a19fbffd9f8e558c1
ogual <ogual82@gmail.com>
2023-10-11 20:43:34 +0200

    Two more tasks

---
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -0,0 +1,30 @@
+import java.lang.StringBuilder;
+
+class SqueakyClean {
+
+    static String clean(String identifier) {
+        StringBuilder result = new StringBuilder(identifier);
+
+        for (int i = 0; i < result.length(); i++) {
+            char current = result.charAt(i);
+            String unicode = Character.UnicodeBlock.of(current).toString();
+
+            if (current == ' ') {
+                result.setCharAt(i, '_');
+            }
+            else if (Character.isISOControl(current)) {
+                result.replace(i, i + 1, "CTRL");
+            }
+            else if (!Character.isLetter(current) || (unicode.equals("GREEK") && Character.isLowerCase(current))) {// handle
+                if (current == '-') {
+                    int next = i + 1;
+                    result.setCharAt(next, Character.toUpperCase(result.charAt(next)));
+                }
+                result.deleteCharAt(i);
+                i--;
+            }
+        }
+        return result.toString();
+    }
+    
+}
